5a908cd9e979b0fd4a3e8c260f593755aee447e5
MonicaIzzo <info@monicaizzo.net>
2023-09-16 08:08:07 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\CharactersController;
+use App\Http\Controllers\MoviesController;
+use App\Http\Controllers\TvController;
+use App\Http\Controllers\GamesController;
+use App\Http\Controllers\CollectiblesController;
+use App\Http\Controllers\VideosController;
+use App\Http\Controllers\FansController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +23,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+// Rotta per pagina HOME
+Route::get('/', [IndexController::class, 'index'])->name('index');
+
+
+// Rotta per pagina CHARACTERS
+Route::get('/characters', [CharactersController::class, 'characters'])->name('characters');
+
+// Rotta per pagina MOVIES
+Route::get('/movies', [MoviesController::class, 'movies'])->name('movies');
+
+
+// Rotta per pagina TV
+Route::get('/tv', [TvController::class, 'tv'])->name('tv');
+
+
+// Rotta per pagina GAMES
+Route::get('/games', [GamesController::class, 'games'])->name('games');
+
+
+// Rotta per pagina COLLCECTIBLE
+Route::get('/collectibles', [CollectiblesController::class, 'collectibles'])->name('collectibles');
+
+
+// Rotta per pagina VIDEOS
+Route::get('/videos', [VideosController::class, 'videos'])->name('videos');
+
+
+// Rotta per pagina FANS
+Route::get('/fans', [FansController::class, 'fans'])->name('fans');
+
+// Rotta per pagina NEWS
+Route::get('/news', [NewsController::class, 'news'])->name('news');
+
+
+// Rotta per pagina SHOP
+Route::get('/shop', [ShopController::class, 'shop'])->name('shop');
+
+
+
+// Rotta per pagina COMICS
+Route::get('/comics/index', [ComicsController::class, 'index'])->name('comics.index'); // mostra la lista dei COMICS
