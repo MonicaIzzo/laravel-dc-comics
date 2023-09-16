74e2b1b1a019e2e39b7bd43491dd6f2e9f92ce49
MonicaIzzo <info@monicaizzo.net>
2023-09-16 08:40:13 +0200

    ADD COMIC seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -12,6 +12,18 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Schema::create('_comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50)->unique();
+            $table->text('description');
+            $table->string('thumb');
+            $table->string('price');
+            $table->string('series');
+            $table->string('sale_date');
+            $table->string('type');
+            $table->text('artists');
+            $table->text('writers');
+            $table->timestamps();
+        });
     }
 }
