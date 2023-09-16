98136825dea438205b60c9962ab07bde63b15d86
MonicaIzzo <info@monicaizzo.net>
2023-09-16 08:07:28 +0200

    config links

---
+++ config/header_links.php
@@ -0,0 +1,13 @@
+<?php return [
+    ['text' => 'INDEX', 'route' => 'index'],
+    ['text' => 'CHARACTERS', 'route' => 'characters'],
+    ['text' => 'COMICS', 'route' => 'comics.index'],
+    ['text' => 'MOVIES', 'route' => 'movies'],
+    ['text' => 'TV', 'route' => 'tv'],
+    ['text' => 'GAMES', 'route' => 'games'],
+    ['text' => 'COLLECTIBLES', 'route' => 'collectibles'],
+    ['text' => 'VIDEOS', 'route' => 'videos'],
+    ['text' => 'FANS', 'route' => 'fans'],
+    ['text' => 'NEWS', 'route' => 'news'],
+    ['text' => 'SHOP', 'route' => 'shop'],
+];
