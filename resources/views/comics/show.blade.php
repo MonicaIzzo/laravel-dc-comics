a691a06a8f913cde59eef188397b21ebcbe16d34
MonicaIzzo <info@monicaizzo.net>
2023-09-16 17:49:04 +0200

    ass REST R

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,17 +1,11 @@
 @extends('layouts.layout')
-
-
 {{-- Setto il titolo della pagina --}}
 @section('title', $comic['title'])
-
 {{-- Setto l'id della pagina --}}
 @section('section-id', 'comic')
-
-
 {{-- Setto il main della pagina (che cambiera in base alla pagina) --}}
 @section('main_content')
     <div class="container" id="main_page">
-
         {{-- COMICS --}}
         <div class="container">
             <div class="container-fluid bg-white" id="comic">
@@ -20,17 +14,16 @@
                         {{-- IMMAGINE --}}
                         <div class="img-comic">
                             <p class="type">COMIC BOOK</p>
-                            <img src="https://static.dc.com/dc/files/default_images/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?w=640"
-                                alt="Action Comics">
+                            <img src={{ $comic['thumb'] }} alt={{ $comic['title'] }}>
                             <p class="gallery">VIEW GALLERY</p>
                         </div>
                     </div>
                     {{-- DETTAGLI --}}
                     <div id="comic-decription" class="mb-3">
-                        <h1>Action Comics #1000: The Deluxe Edition"</h1>
+                        <h1 class="text-uppercase">{{ $comic['title'] }}</h1>
                         <section id="prince">
                             <div class="prince_sx">
-                                <p class="peince">Prezzo: $19.99</p>
+                                <p class="peince">{{ $comic['price'] }}</p>
                                 <p class="peince">AVAILABLE</p>
                             </div>
                             <div class="prince_dx">
@@ -38,27 +31,8 @@
                             </div>
                         </section>
                         {{-- DESCRIZIONE --}}
-                        <p class="description">The celebration of 1,000 issues of Action Comics continues with a new, Deluxe
-                            Edition
-                            of
-                            the amazing comic
-                            that won raves when it hit comics shops in April! This hardcover includes all the stories from
-                            that
-                            issue,
-                            plus the tale by writer Paul Levitz and artist Neal Adams that appeared in the Action Comics: 80
-                            Years
-                            Of
-                            Superman hardcover, as well as all the variant covers, design sketches by Jim Lee for Superman’s
-                            new
-                            look,
-                            scripts for the stories, the original art from the lost story featuring art by Curt Swan and
-                            more! Plus:
-                            a
-                            complete reprint of the stories that started it all—the Superman stories Action Comics #1 and 2
-                            from
-                            1938!
+                        <p class="description">{{ $comic['description'] }}
                         </p>
-
                         <section id="footer">
                             {{-- COLONNA SX --}}
                             <div id="talent">
@@ -68,17 +42,7 @@
                                         <h3 class="text-primary">Art by:</h3>
                                     </div>
                                     <div class="column-2">
-                                        <h5>"José Luis García-López",
-                                            "Clay Mann",
-                                            "Rafael Albuquerque",
-                                            "Patrick Gleason",
-                                            "Dan Jurgens",
-                                            "Joe Shuster",
-                                            "Neal Adams",
-                                            "Curt Swan",
-                                            "John Cassaday",
-                                            "Olivier Coipel",
-                                            "Jim Lee"</h5>
+                                        <h5>{{ $comic['artists'] }}"</h5>
                                     </div>
                                 </div>
                                 <div class="row">
@@ -86,24 +50,10 @@
                                         <h3 class="text-primary">Written by:</h3>
                                     </div>
                                     <div class="column-2">
-                                        <h5>"Brad Meltzer",
-                                            "Tom King",
-                                            "Scott Snyder",
-                                            "Geoff Johns",
-                                            "Brian Michael Bendis",
-                                            "Paul Dini",
-                                            "Louise Simonson",
-                                            "Richard Donner",
-                                            "Marv Wolfman",
-                                            "Peter J. Tomasi",
-                                            "Dan Jurgens",
-                                            "Jerry Siegel",
-                                            "Paul Levitz"</h5>
+                                        <h5>{{ $comic['writers'] }}</h5>
                                     </div>
                                 </div>
                             </div>
-
-
                             {{-- COLONNA DX --}}
                             <div id="specs">
                                 <h2 class="text-primary">Specs</h2>
@@ -112,34 +62,29 @@
                                         <h3 class="text-primary">Series:</h3>
                                     </div>
                                     <div class="column-2">
-                                        <h5 class="text-primary">ACTION COMICS</h5>
+                                        <h5 class="text-primary text-uppercase">{{ $comic['series'] }}</h5>
                                     </div>
                                 </div>
-
                                 <div class="row">
                                     <div class="column-1">
                                         <h3>U.S. Prezzo:</h3>
                                     </div>
                                     <div class="column-2">
-                                        <h5>$19.99</h5>
+                                        <h5>{{ $comic['price'] }}</h5>
                                     </div>
                                 </div>
-
-
                                 <div class="row">
                                     <div class="column-1">
                                         <h3>On Sale Date:</h3>
                                     </div>
                                     <div class="column-2">
-                                        <h5>2018-10-02</h5>
+                                        <h5>{{ $comic['sale_date'] }}</h5>
                                     </div>
                                 </div>
 
                             </div>
                     </div>
-
                 </div>
-
             </div>
         </div>
         <div class="load text-center">
